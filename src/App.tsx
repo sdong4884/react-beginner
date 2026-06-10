29b2f0f9ba78efcd5d1d032c6d3304ac7ad7cb5c
sdong4884 <sdong4884@gmail.com>
2026-06-10 15:22:20 +0900

    constants 상수화를 통한 Side-bar 컴포넌트 만들기

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import { AppHeader } from "./components/common";
+import { AppSidebar } from "./components/common/AppSidebar";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div className="page">
         <AppHeader />
-        <div className="container"></div>
+        <div className="container">
+          <main className="w-full h-full min-h-180 flex p-6 gap-6">
+            <AppSidebar />
+            <section className="flex-1 flex flex-col gap-12"></section>
+          </main>
+        </div>
       </div>
     </ThemeProvider>
   );
